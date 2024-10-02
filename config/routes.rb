4e508c2d55b3ba4fb4d7b85fa33f273f00e25510
Rodrigo Toledo <rodrigotoledo@Rodrigos-MacBook-Pro.local>
2024-10-02 11:40:14 -0300

    Add letter opener gem and create registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get "home", to: "home#index", as: :home
+  resource :registrations, only: %i[create new]
   resource :session
   resources :passwords, param: :token
-  root "home#index"
+  root "protected#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
