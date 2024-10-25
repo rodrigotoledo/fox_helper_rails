f4a3aa60748f5cc3c52896876295f5c0ea931f97
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2024-10-25 15:31:22 -0300

    Update ActiveRecord schema version to 8.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_02_094728) do
+ActiveRecord::Schema[8.1].define(version: 2024_10_02_094728) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
